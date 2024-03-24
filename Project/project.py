064dbf00c3ba2d766591c42a11dcf7ffc3b0c32d
Muhammad Kumail <155740227+Kumail2003@users.noreply.github.com>
2024-03-24 13:35:09 +0500

    Project

--- Project/project.py
+++ Project/project.py
@@ -1,9 +1,12 @@
 import streamlit as st
 import pandas as pd
 
-def read_data_from_csv(filename):
-    data = pd.read_csv(filename, encoding='latin-1')
-    return data
+def read_data_from_csv(filename, encoding='utf-8'):
+    try:
+        data = pd.read_csv(filename, encoding=encoding)
+        return data
+    except Exception as e:
+        st.error(f"Error reading CSV file: {e}")
 
 def search(keyword, data):
     filtered_data = data[data.apply(lambda row: keyword.lower() in str(row).lower(), axis=1)]
@@ -14,20 +17,19 @@ def main():
 
     data_from_scraped_data_csv = read_data_from_csv('Project/scraped_data.csv')
 
-    st.write("## Data")
+    if data_from_scraped_data_csv is not None:
+        st.write("## Data")
+        st.write(data_from_scraped_data_csv.head(10))
 
-    # Display the first 10 rows of the data
-    st.write(data_from_scraped_data_csv.head(10))
+        st.write("## Search")
 
-    st.write("## Search")
-
-    keyword = st.text_input("Enter keyword:")
-    if st.button("Search"):
-        filtered_data = search(keyword, data_from_scraped_data_csv)
-        if filtered_data.empty:
-            st.write("No results found.")
-        else:
-            st.write(filtered_data)
+        keyword = st.text_input("Enter keyword:")
+        if st.button("Search"):
+            filtered_data = search(keyword, data_from_scraped_data_csv)
+            if filtered_data.empty:
+                st.write("No results found.")
+            else:
+                st.write(filtered_data)
 
 if __name__ == "__main__":
     main()
