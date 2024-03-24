9eaac4d956ff7cbef63f3d9cbcdfa5e1d8bed526
Muhammad Kumail <155740227+Kumail2003@users.noreply.github.com>
2024-03-24 13:18:21 +0500

    Project

--- Project/project.py
+++ Project/project.py
@@ -2,16 +2,7 @@ import streamlit as st
 import pandas as pd
 
 def read_data_from_csv(filename):
-    try:
-        data = pd.read_csv(filename, encoding='utf-8')
-        print("CSV file successfully loaded.")
-        return data
-    except FileNotFoundError:
-        print(f"Error: File '{filename}' not found.")
-        return None
-    except Exception as e:
-        print(f"Error: {e}")
-        return None
+    return pd.read_csv(filename)
 
 def main():
     st.title('Scraped Data Viewer')
@@ -21,7 +12,7 @@ def main():
 
     # Display the top 10 data entries in a table
     st.write('## Top 10 Data Entries')
-    st.write(data_from_scraped_data_csv.head(10))
+    st.table(data_from_scraped_data_csv.head(10))
 
     # Search functionality
     st.sidebar.title("Search")
@@ -29,8 +20,11 @@ def main():
     if keyword:
         filtered_data = data_from_scraped_data_csv[data_from_scraped_data_csv.apply(lambda row: keyword.lower() in row.astype(str).str.lower().values, axis=1)]
         if not filtered_data.empty:
-            st.write('## Search Results')
-            st.write(filtered_data)
+            st.write('## Search Results (Tabular Format)')
+            st.table(filtered_data)
+
+            st.write('## Search Results (JSON Format)')
+            st.json(filtered_data.to_dict(orient='records'))
         else:
             st.write("No results found for the given keyword.")
 
